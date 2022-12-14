23a1907a4144247a3a28248175cae9dcc6282ec4
Milacover <117834108+Milacover@users.noreply.github.com>
2022-12-14 10:02:47 +0100

    change rama feature/create-home

---
+++ index.js
@@ -0,0 +1,7 @@
+document.write('<a href=\"http://127.0.0.1:5500/index-juego.html\">START</a>');
+document.querySelector(".big-button").style.color="black";
+document.querySelector(".big-button").style.padding="10px";
+document.querySelector("h1").style.color="black"; 
+document.querySelector("h1").style.fontSize = "xx-large";
+document.querySelector("h2").style.fontSize = "2cm";
+document.getElementById("h2").style.fontFamily = "Time Roman";

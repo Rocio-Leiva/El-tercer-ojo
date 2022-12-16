0c3298afafcc379f0797ae75f693cf7626f5c19b
María del Carmen Trillo <mctrillon@gmail.com>
2022-12-16 10:16:54 +0100

    updated tests

---
+++ addCard.js
@@ -0,0 +1,12 @@
+// let codersList = [("Ainoha", "Acuario"),("Mila", "Acuario"),("Delia","Aries")];
+
+// newList=[]
+// function addCoders(array){
+//     var codersList;                                 
+//     for (var i = 0; i < 25; i++) {               
+//         codersList[i] = document.getElementById("input"+(i+1)).value;
+//         newList.push(coderList[i]);  
+//         }
+//     console.log(newList)     
+// }
+

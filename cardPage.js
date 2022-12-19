354d993a2ca306779833f7764b5fabd8761c0ab9
Rocio-Leiva <rmleipec@gmail.com>
2022-12-19 10:52:45 +0100

    added changes

--- cardPage.js
+++ cardPage.js
@@ -1,41 +1,13 @@
-let seleccionar = document.querySelector('select');
-let formulario = document.querySelector('.play');
+import { codersList } from "./data.js"
 
-// seleccionar.addEventListener('change', seleccionarHoroscopo());
+//coderList array 25 alumnas
+//array fusionado newList
 
-function seleccionarHoroscopo() {
-  let eleccion = seleccionar.value;
-console.log(eleccion);
-  if (eleccion === 'Piscis') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Aries') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.factoriaf5.org/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Tauro') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Geminis') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Cancer') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Leo') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Virgo') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Libra') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Escorpio') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Sagitario') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Capricornio') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } else if (eleccion === 'Acuario') {
-    formulario.innerHTML = "<button type='submit' class='btn btn-primary'><a class='button-link' href='https://www.amazon.es/'>SABER MI FUTURO</a></button>";
-  } 
- 
-}
+//si newList = 0 que no se ha metido datos,, se hace el random de coderslist teniendo en cuenta lengt de coderslist y si el new list es distinto de cero se coge el ramdon de newlist
 
 const horoscopos = ["Piscis", "Aries", "Tauro", "Geminis", "Cancer", "Leo", "Virgo", "Libra", "Escorpio", "Sagitario", "Capricornio", "Acuario" ];
-const aleatorio = horoscopos[Math.floor(Math.random() * horoscopos.length)];
+// let coderList o newlist
+const aleatorio = horoscopos[Math.floor(Math.random() * codersList.length)];
 let boton = document.querySelector(".suerte");
 
 boton.addEventListener("click", ()=> {

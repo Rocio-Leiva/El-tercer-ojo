bcebe21e950b2c12f2acaf0d8da05c0f29ce8773
Carmen-Trillo <mctrillon@gmail.com>
2022-12-19 11:53:40 +0100

    added images sign array

--- addCard.js
+++ addCard.js
@@ -2,98 +2,122 @@ let codersList =
     [{
     name:'Ainoha',
     sign: 'Acuario',
+    img: './img/acuario.png',
     },
     {
     name:'Mila',
     sign:'Acuario',
+    img: './img/acuario.png',
     },
     {
     name:'Delia',
     sign:'Aries',
+    img: './img/aries.png',
     },
     {
     name: 'Marta',
     sign: 'Aries',
+    img: './img/aries.png',
     },
     {
     name: 'Miriam',
     sign: 'Cáncer',
+    img: './img/cancer.png',
     },
     {
     name: 'Claudette',
     sign: 'Capricornio',
+    img: './img/capricornio.png',
     },
     {
     name: 'Paola',
     sign: 'Capricornio',
+    img: './img/capricornio.png',
     },
     {
     name: 'Vero',
     sign: 'Capricornio',
+    img: './img/capricornio.png',
     },
     {
     name: 'Aldara',
     sign: 'Escorpio',
+    img: './img/escorpio.png',
     },
     {
     name: 'Ana',
     sign: 'Escorpio',
+    img: './img/escorpio.png',
     },
     {
     name: 'Margarita',
     sign: 'Géminis',
+    img: './img/geminis.png',
     },
     {
     name: 'Nohely',
     sign: 'Géminis',
+    img: './img/geminis.png',
     },
     {
     name: 'Anyi',
     sign: 'Leo',
+    img: './img/leo.png',
     },
     {
     name: 'Mary',
     sign: 'Leo',
+    img: './img/leo.png',
     },
     {
     name: 'Celia',
     sign: 'Libra',
+    img: './img/libra.png',
     },
     {
     name: 'Maria Shirley',
     sign: 'Libra',
+    img: './img/libra.png',
     },
     {
     name: 'Cele',
     sign: 'Piscis',
+    img: './img/piscis.png',
     },
     {
     name: 'Diana',
     sign: 'Piscis',
+    img: './img/piscis.png',
     },
     {
     name: 'Rocio',
     sign: 'Piscis',
+    img: './img/piscis.png',
     },
     {
     name: 'Rosibel',
     sign: 'Piscis',
+    img: './img/piscis.png',
     },
     {
     name: 'Carmen',
     sign: 'Sagitario',
+    img: './img/sagitario.png',
     },
     {
     name: 'Eva',
     sign: 'Virgo',
+    img: './img/virgo.png',
     },
     {
     name: 'Jennifer',
     sign: 'Virgo',
+    img: './img/virgo.png',
     },
     {
     name: 'Rosemary',
     sign: 'Virgo',
+    img: './img/virgo.png',
     }
     ];    
 

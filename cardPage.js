37a96c1d885942c6737d3975962abf720b44f171
Carmen-Trillo <mctrillon@gmail.com>
2022-12-20 12:13:41 +0100

    card animation

--- cardPage.js
+++ cardPage.js
@@ -1,6 +1,8 @@
 import { codersList } from "./data.js"
 import { newBaseDate } from "./addCard.js"
 
+let card_selection = document.querySelector("#card_selection");
+
 let boton = document.querySelector(".suerte");
 let eleccion = "";
 boton.addEventListener("click", ()=> {
@@ -11,9 +13,15 @@ let signName = newList.map(element => element ["name"]);
 let horoscopo = newList.map(element => element ["sign"]);
 let aleatorio = Math.floor(Math.random() * newList.length);
 
+card_selection.innerHTML=`<div class="d-flex flex-column align-items-center justify-content-center"><h1 class="name_animation">${signName[aleatorio]}</h1>
+      <div class="d-felx align-items-center justify-content-center"><img src="${signImagen[aleatorio]} class="justify-content-center card_animation" alt="... ">
+      </div>
+      </div>`
+
 console.log(newList[aleatorio]);
 let eleccion = horoscopo[aleatorio];
 console.log(eleccion);
+
 
 azar();
 });
@@ -28,23 +36,23 @@ function azar() {
     
     // window.location = "<button type='button' class='btn btn-primary btn-lg suerte'><a class='button-link' href='https://www.amazon.es/'>PROBAR SUERTE</a></button>";
   } else if (eleccion === [1] === 'Aries') {
-    window.location = "url2";
+    window.location = href='https://www.amazon.es/';
   } else if (eleccion === [1] === 'Tauro') {
-    window.location = "url3" ;
+    window.location = href='https://www.amazon.es/' ;
   } else if (eleccion === [1] === 'Geminis') {
-    window.location = "url4";
+    window.location = href='https://www.amazon.es/';
   } else if (eleccion === [1] === 'Cancer') {
-    window.location = "url5";
+    window.location = href='https://www.amazon.es/';
   } else if (eleccion === [1] === 'Leo') {
-    window.location = "url6";
+    window.location = href='https://www.amazon.es/';
   } else if (eleccion === [1] === 'Virgo') {
-    window.location = "url7";
+    window.location = href='https://www.amazon.es/';
   } else if (eleccion === [1] === 'Libra') {
-    window.location = "url8" ;
+    window.location = href='https://www.amazon.es/' ;
   } else if (eleccion === [1] === 'Escorpio') {
-    window.location = "url9";
+    window.location = href='https://www.amazon.es/';
   } else if (eleccion === [1] ===  'Sagitario') {
-    window.location = "url10";
+    window.location = href='https://www.amazon.es/';
   } else if (eleccion === [1] === 'Capricornio') {
     window.location = "url11";
   // } else if (eleccion === 'Acuario') {

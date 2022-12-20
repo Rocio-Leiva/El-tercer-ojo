dfcd7fba2330d99ad565ece2462d4bd098a47f91
Rocio-Leiva <rmleipec@gmail.com>
2022-12-20 20:19:22 +0100

    added new line

--- cardPage.js
+++ cardPage.js
@@ -13,9 +13,10 @@ let signName = newList.map(element => element ["name"]);
 let horoscopo = newList.map(element => element ["sign"]);
 let aleatorio = Math.floor(Math.random() * newList.length);
 
-card_selection.innerHTML=`<div class="d-flex flex-column align-items-center justify-content-center"><h1 class="name_animation">${signName[aleatorio]}</h1>
+card_selection.innerHTML=`<div class="d-flex flex-column align-items-center justify-content-center"><h1 style="color:#ffffff; text-align: center;
+class="name_animation">${signName[aleatorio]}</h1>
       <div class="d-flex align-items-center justify-content-center"><img src="${signImagen[aleatorio]}" class="justify-content-center 
-      card_animation" alt="... ">
+      card_animation" alt="... " width="180rem" height="280rem">
       <button type="button" class="btn btn-info"><a href="${horoscopo[aleatorio]}.html">Ir a mi horoscopo</a></button>
 
       </div>

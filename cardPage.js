6794a7f832095e4aeb10ce9e3697ac7d30212f0e
Rocio-Leiva <rmleipec@gmail.com>
2022-12-21 14:28:25 +0100

    added changes

--- cardPage.js
+++ cardPage.js
@@ -11,13 +11,19 @@ boton.addEventListener("click", ()=> {
 let signImagen = newList.map(element => element ["img"]);
 let signName = newList.map(element => element ["name"]);
 let horoscopo = newList.map(element => element ["sign"]);
-let aleatorio = Math.floor(Math.random() * newList.length);
+let i,k,aleatorio;
+    for (i = newList.length; i; i--) {
+        aleatorio = Math.floor(Math.random() * i);
+        k = newList[i - 1];
+        newList[i - 1] = newList[aleatorio];
+        newList[aleatorio] = k;}
+// let aleatorio = Math.floor(Math.random() * newList.length);
 
-card_selection.innerHTML=`<div class="d-flex flex-column align-items-center justify-content-center"><h1 style="color:#ffffff; text-align: center;
+card_selection.innerHTML=`<div class="d-flex flex-column align-items-center justify-content-center"><h1 style="color:#ffffff; font-family:'Love'; text-align: center;
 class="name_animation">${signName[aleatorio]}</h1>
       <div class="d-flex align-items-center justify-content-center"><img src="${signImagen[aleatorio]}" class="justify-content-center 
       card_animation" alt="...  width="100rem" height="170rem" > </div>
-      <button type="button" class="d-flex align-items-center justify-content-center btn btn-info"><a href="${horoscopo[aleatorio]}.html">Ir a mi horoscopo</a></button>
+      <button type="button" id= "button-link" class="d-flex align-items-center justify-content-centerbtn btn-primary btn-sm"><a href="${horoscopo[aleatorio]}.html">IR A MI HOROSCOPO</a></button>
 
      
       </div>`

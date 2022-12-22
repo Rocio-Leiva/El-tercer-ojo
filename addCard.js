7592c33d0a28538ef912fdeec6d22222ed6ce5a4
Rocio-Leiva <rmleipec@gmail.com>
2022-12-22 09:12:06 +0100

    added some changes

--- addCard.js
+++ addCard.js
@@ -22,18 +22,18 @@ function addCoders() {
     signAdd = document.querySelector('select').value;
     imgAsign='';
          switch(signAdd){
-                case "Acuario": imgAsign = './img/acuario.png'; break;
-                case "Cancer": imgAsign = './img/cancer.png'; break;
-                case signAdd="Aries": imgAsign = './img/aries.png'; break;
-                case signAdd="Capricornio": imgAsign = './img/capricornio.png'; break;
-                case signAdd="Escorpio": imgAsign = './img/escorpio.png'; break;
-                case signAdd="Géminis": imgAsign = './img/geminis.png'; break;
-                case signAdd="Leo": imgAsign = './img/leo.png'; break;
-                case signAdd="Libra": imgAsign = './img/libra.png'; break;
-                case signAdd="Piscis": imgAsign = './img/piscis.png'; break;
-                case signAdd="Sagitario": imgAsign = './img/sagitario.png'; break;
-                case signAdd="Tauro": imgAsign = './img/tauro.png'; break;
-                case signAdd="Virgo": imgAsign = './img/virgo.png'; break;
+                case "Acuario": imgAsign = './img/acuario.jpg'; break;
+                case "Cancer": imgAsign = './img/cancer.jpg'; break;
+                case "Aries": imgAsign = './img/aries.jpg'; break;
+                case "Capricornio": imgAsign = './img/capricornio.jpg'; break;
+                case "Escorpio": imgAsign = './img/escorpio.jpg'; break;
+                case "Géminis": imgAsign = './img/geminis.jpg'; break;
+                case "Leo": imgAsign = './img/leo.jpg'; break;
+                case "Libra": imgAsign = './img/libra.jpg'; break;
+                case "Piscis": imgAsign = './img/piscis.jpg'; break;
+                case "Sagitario": imgAsign = './img/sagitario.jpg'; break;
+                case "Tauro": imgAsign = './img/tauro.jpg'; break;
+                case "Virgo": imgAsign = './img/virgo.jpg'; break;
              }; 
     
     console.log();
